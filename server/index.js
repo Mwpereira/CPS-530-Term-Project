cebe019fd7f72c2c3194f5430a7bad7ca5a7c90a
Michael Pereira <michael.w.pereira.7@gmail.com>
2021-12-02 16:49:00 -0500

    Adding heroku process env for port number

--- server/index.js
+++ server/index.js
@@ -3,23 +3,21 @@ const cors = require('cors');
 const axios = require('axios');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors());
 
 const apiKey = 'e76639a2f11fd1a42584ba9908864bda';
 
 // Get weather info
 app.get('/', (req, res) => {
-	axios
-		.get(
-			`https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}`,
-		)
-		.then((weatherInfo) => {
-			res.send(weatherInfo.data);
-		})
-		.catch((e) => console.log(e));
+    axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=${apiKey}`)
+        .then((weatherInfo) => {
+            res.send(weatherInfo.data);
+        })
+        .catch((e) => console.log(e));
 });
 
 app.listen(port, (req, res) => {
-	console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
